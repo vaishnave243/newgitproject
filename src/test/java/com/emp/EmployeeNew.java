fe608f4b16a187f54fd0fa7a35134dcce5b37b2a
vaishu <vaishnave243@gmail.com>
2025-07-27 20:01:41 +0530

    new git project start

---
+++ src/test/java/com/emp/EmployeeNew.java
@@ -0,0 +1,27 @@
+package com.emp;
+
+public class EmployeeNew 
+{
+
+	public void state() {
+		System.out.println("Tamilnadu");
+	}
+		
+	public void name() {
+		System.out.println("vaishu");
+
+	}
+
+	public void address() {
+		System.out.println("coimbatore");
+	}
+
+	public void officename() {
+		System.out.println("TCS");
+	}
+
+	public void officeaddress() {
+		System.out.println("chennai");
+	}
+
+}
